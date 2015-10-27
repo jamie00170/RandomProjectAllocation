ce9f86ddc3f4365825e9dc0af32f6f51acd6e12c
jamie00170 <jamiedd13@aol.com>
2015-10-27 09:57:58 +0000

    Fix removeMatched method

--- src/Main.java
+++ src/Main.java
@@ -126,6 +126,7 @@ public class Main {
             student_list.add(name);
 
         }
+
         for (String name : student_list) {
             if (student_allocation.get(name) == 1.0) {
                 student_preferences.remove(name);
@@ -133,30 +134,27 @@ public class Main {
 
         }
 
-        ArrayList<String> project_list = new ArrayList<>();
-        for (String project : project_allocation.keySet()){
-            project_list.add(project);
-        }
-        for (String name : student_list) {
-            ArrayList<String> preferences = student_preferences.get(name);
-            int i = 0;
-            while (i < preferences.size()) {
-                if (project_allocation.get(preferences.get(i)) == 1.0) {
-                    // remove project from all preferences list
-                    System.out.println("Removing " + preferences.get(i));
-                    preferences.remove(preferences.get(i));
+        ArrayList<String> items_to_remove = new ArrayList<>();
 
-                    i--;
+        for (String name : student_preferences.keySet()) {
+            // Get students list of ranked choices
+            ArrayList<String> preferences = student_preferences.get(name);
+            // Loop through their choices
+            for (String project : preferences) {
+                // If the choice has been fully matched remove it from the preferences list
+                if (project_allocation.get(project) == 1.0) {
+                    // remove project from preferences list
+                    System.out.println("Removing " + project);
+                    items_to_remove.add(project);
                 }
-            i++;
             }
+            preferences.removeAll(items_to_remove);
         }
         System.out.println("Removed matched students and projects");
         }
 
 
-
-    public static void probabilisticSerialDictatorship(Map student_preferences, ArrayList project_list){
+    public static void probabilisticSerialDictatorship(Map<String, ArrayList<String>> student_preferences, ArrayList project_list){
 
         // Stores the amount left of each project to be consumed
         Map<String, Double> project_allocation = new HashMap<>();
@@ -200,21 +198,25 @@ public class Main {
         }
 
         while(check_sizes(project_allocation) || check_sizes(student_allocation)){
-
+            // loop for number of students or projects - whatever there is more of
             while (j < num_of_iterations){
-
                 // Get current projects being consumed by students
                 Map<String, Double> currentProjects = getCurrentProjects(j, student_preferences, current_projects, students, num_of_iterations);
                 System.out.println("Current Projects: " + currentProjects.toString());
 
-                for (String name : students){
+                // for each student that hasn't yet been matched
+                for (String name : student_preferences.keySet()){
+                    // get the students list of preferences
                     ArrayList<String> preferences = (ArrayList<String>) (student_preferences.get(name));
-                    // increment student allocation + project allocation
-                    String current_project = preferences.get(j);
+                    // get their first available choice - matched choices have been removed to always index 0
+                    String current_project = preferences.get(0);
+                    // Find out the number of student consuming this students current project
                     Double num_of_students_consuming = current_projects.get(current_project);
-                    // have a method that gets the amount of each project remaining before loop so it doesn't change during the iteration
+                    // Find the amount of the project yet to be assigned
                     Double amount_of_project_remaining = (1.0 - (projects_remaining.get(current_project)));
 
+                    //System.out.println(name +" has: " + (amount_of_project_remaining/num_of_students_consuming) + "percenatge of being matched to" + current_project);
+                    // increment student allocation + project allocation
                     student_allocation.put(name, student_allocation.get(name) + (amount_of_project_remaining / num_of_students_consuming));
                     project_allocation.put(current_project, project_allocation.get(current_project) + (amount_of_project_remaining/num_of_students_consuming));
 
@@ -231,8 +233,8 @@ public class Main {
                 // increment appropriate students and projects
                 // remove matched students + projects from student preferences
 
-                //j++;
-                break;
+                j++;
+                //break;
                 }
                 break;
                 // get each students first choice run clock, stop when one is exhausted
