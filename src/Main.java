e20593470572eada0f8359299ad2ead7017ce1d7
jamie00170 <jamiedd13@aol.com>
2016-03-03 14:44:52 +0000

    Fix error in stringToFraction method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-import org.apache.commons.math.fraction.Fraction;
-import org.apache.commons.math.fraction.FractionConversionException;
+import org.apache.commons.math3.fraction.Fraction;
+import org.apache.commons.math3.fraction.FractionConversionException;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -39,35 +39,39 @@ public class Main {
 
         Fraction f;
 
-        if (fraction_string.length() == 5){
-            Double numerator = Double.parseDouble(fraction_string.substring(0,1));
-            Double denominator = Double.parseDouble(fraction_string.substring(4));
+        fraction_string = fraction_string.replaceAll("\\s","");
+        String[] data = fraction_string.split("/");
+        System.out.println("string split:" + Arrays.toString(data));
 
-            Double fraction_value = numerator/denominator;
+
+        if (data.length > 1) {
+            Double numerator = Double.parseDouble(data[0]);
+            Double denominator = Double.parseDouble(data[1]);
+
+            Double fraction_value = numerator / denominator;
+
 
             try {
                 f = new Fraction(fraction_value);
                 return f;
-            } catch (FractionConversionException e ){
+            } catch (FractionConversionException e) {
                 e.printStackTrace();
             }
+        }else{
 
-        }else if (fraction_string.length() == 1){
-
+            Double fraction_value = Double.parseDouble(data[0]);
             try {
-                Double fraction_value = Double.parseDouble(fraction_string);
-
                 f = new Fraction(fraction_value);
                 return f;
             } catch (FractionConversionException e) {
                 e.printStackTrace();
             }
         }
-        // Need to add clauses for different length numerators and denominators i.e. 19/20
+
+
         System.out.println("FRACTION NOT TRANSFORMED TO STRING");
         return new Fraction(0);
     }
-
 
 
     public static int[] getCoordinates(String[][] matrix, String student, String project){
--- src/stringToFractionTest.java
+++ src/stringToFractionTest.java
@@ -1,8 +1,10 @@
 /**
  * Created by Jamie on 03/03/2016.
  */
-import org.apache.commons.math.fraction.Fraction;
-import org.apache.commons.math.fraction.FractionConversionException;
+import org.apache.commons.math3.fraction.Fraction;
+import org.apache.commons.math3.fraction.FractionConversionException;
+
+import java.util.Arrays;
 
 public class stringToFractionTest {
 
@@ -10,31 +12,22 @@ public class stringToFractionTest {
 
         Fraction f;
 
-        if (fraction_string.length() == 5){
-            Double numerator = Double.parseDouble(fraction_string.substring(0,1));
-            Double denominator = Double.parseDouble(fraction_string.substring(4));
+        fraction_string = fraction_string.replaceAll("\\s","");
+        String[] data = fraction_string.split("/");
+        System.out.println("string split:" + Arrays.toString(data));
 
-            Double fraction_value = numerator/denominator;
+        Double numerator = Double.parseDouble(data[0]);
+        Double denominator = Double.parseDouble(data[1]);
 
-            try {
-                f = new Fraction(fraction_value);
-                return f;
-            } catch (FractionConversionException e ){
-                e.printStackTrace();
-            }
+        Double fraction_value = numerator/denominator;
 
-        }else if (fraction_string.length() == 1){
-
-            try {
-                Double fraction_value = Double.parseDouble(fraction_string);
-
-                f = new Fraction(fraction_value);
-                return f;
-            } catch (FractionConversionException e) {
-                e.printStackTrace();
-            }
+        try {
+            f = new Fraction(fraction_value);
+            return f;
+        } catch (FractionConversionException e ){
+            e.printStackTrace();
         }
-        // Need to add clauses for different length numerators and denominators i.e. 19/20
+
         System.out.println("FRACTION NOT TRANSFORMED TO STRING");
         return new Fraction(0);
     }
